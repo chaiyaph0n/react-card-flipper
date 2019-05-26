ddd6a919976a5bfd47f0200c969f7a26ba652781
chaiyaph0n <ball.chaiyaphon@gmail.com>
2019-05-26 23:31:51 +0700

    first commit - add 4 card animate (left, right, top, down)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react';
+import classNames from 'classnames'
+import './App.scss';
+
+function CardFlipper({direction = 'right', isFlipped, handlerOnClick}) {
+  return (
+    <div className="card-flipper-wrapper">
+      <div className={classNames(
+          'card-flipper',
+          `flip-${direction}`,
+          { flipped: isFlipped })
+        }
+      >
+        <div className="card card-front" onClick={handlerOnClick(true)}>front</div>
+        <div className="card card-back" onClick={handlerOnClick(false)}>back</div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
+  const [flipped, setFlipped] = useState(false)
+
+  const _onClick = useCallback(
+    isFlipped => () => {
+      setFlipped(() => isFlipped)
+    },
+    []
+  )
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardFlipper direction={'left'} isFlipped={flipped} handlerOnClick={_onClick} />
+      <CardFlipper direction={'right'} isFlipped={flipped} handlerOnClick={_onClick} />
+      <CardFlipper direction={'top'} isFlipped={flipped} handlerOnClick={_onClick} />
+      <CardFlipper direction={'bottom'} isFlipped={flipped} handlerOnClick={_onClick} />
     </div>
   );
 }
